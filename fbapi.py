a9ce9ab6aefb06bbe0e8acede2c8bf31e61245aa
Artemis <artemis@artemis.cat>
2015-03-26 17:04:26 -0700

    Replace the official Facebook SDK by facepy

--- fbapi.py
+++ fbapi.py
@@ -1,4 +1,5 @@
-import facebook
+import facepy
+from urlparse import parse_qs
 
 # get_login_url returns the Facebook URL the user should be redirected to to
 # obtain a Facebook Graph token
@@ -8,10 +9,9 @@ def get_login_url(client_id, redirect_uri):
 
 # get_facebook_response does the Facebook Graph request to Facebook
 def get_facebook_response(client_id, client_secret, redirect_uri, user_code):
-    graph = facebook.GraphAPI()
-    token = graph.get_access_token_from_code(user_code, redirect_uri, client_id,
-            client_secret)
-    graph = facebook.GraphAPI(access_token=token.itervalues().next(),
-        version='2.2')
-    return graph.get_object(
-            id='me/taggable_friends?fields=name,picture.type(large)')
+    graph = facepy.GraphAPI()
+    response = graph.get(path="oauth/access_token", client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri, code=user_code)
+    data = parse_qs(response)
+    graph = facepy.GraphAPI(data['access_token'][0])
+    return graph.get(path="me/taggable_friends?fields=name,picture.type(large)")
+
